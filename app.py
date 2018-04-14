ad17ded66efd4d99639cbef62c08c042ebfd6d3f
kyttypr <julettazhuk@gmail.com>
2018-04-15 00:45:55 +0700

    first steps

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from config import Config
+from forms import LoginForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 
 @app.route('/index')
 @app.route('/')
@@ -25,9 +27,34 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/actors')
+def show_actors():
+    user = {'username': 'Эльдар Рязанов'}
+    actors = [
+        {
+            'name': 'John Smith',
+            'genre': 'Drama actor'
+        },
+        {
+            'name': 'Anna Blake',
+            'genre': 'Drama actor'
+        },
+        {
+            'name': 'Susann Khils',
+            'genre': 'Comedy actor'
+        }
+    ]
+    return render_template('actors.html', title='Actors', user=user, actors=actors)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == '__main__':
